8e1293f60d119e18f1e3ca3e149870cf1c4080b9
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 17:07:37 +0200

    read app_secret from vault

---
+++ main.tf
@@ -0,0 +1,3 @@
+data "vault_generic_secret" "app_secret" {
+  path = "kv/app_secret"
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "app_secret" {
+  value = "${data.vault_generic_secret.app_secret.data["username"}"
+}
