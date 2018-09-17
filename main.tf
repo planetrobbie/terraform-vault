46cbef57a391eb12cd0169305f2fd8c60952cf0b
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 18:07:58 +0200

    main fixed again

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 data "vault_generic_secret" "app_secret" {
   path = "kv/app_secret"
 }
-
-data "template_file" "credentials" {
-  template = "username: $${username}}\npassword: $${{password}}"
-  vars {
-    username = "${data.vault_generic_secret.app_secret.data["username"]}"
-    password = "${data.vault_generic_secret.app_secret.data["password"]}"
-  }
-}
