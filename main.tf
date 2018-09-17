e9d9ab30bc463ad7a27053e44ab99aa38ccf0a43
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 18:12:18 +0200

    policy added

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 data "vault_generic_secret" "app_secret" {
   path = "kv/app_secret"
 }
+
+# Adding a policy
+source "vault_policy" "policy_from_terraform_provider" {
+  name = "policy_from_terraform_provider"
+
+  policy = <<EOF
+path "kv/*" {
+        policy = "read"
+}
+EOF
+}
