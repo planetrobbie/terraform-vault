df444d7de634149b61738aa31a0dc45893045571
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 17:35:41 +0200

    now using templating data source

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 data "vault_generic_secret" "app_secret" {
   path = "kv/app_secret"
 }
+
+data "template_file" "credentials" {
+  template = "username: $${username}}\n$$password: {{password}}"
+
+  vars {
+    username  = "{vault_generic_secret.app_secret.data["username"}"
+    password  = "{vault_generic_secret.app_secret.data["password"}"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "app_secret" {
+  description = "secret out of our Vault"
   value = "${data.vault_generic_secret.app_secret.data["username"]}"
 }
+
+output "config_file {
+  description = "rendered template which consume Vault secrets"
+  value = "${data.template_file.credentials.rendered}"
+}`  
