e5b1c74da2570290bea48ef231024b531a773c9e
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 17:51:42 +0200

    main fixed again

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ data "vault_generic_secret" "app_secret" {
 }
 
 data "template_file" "credentials" {
-  template = "username: $${data.vault_generic_secret.app_secret.data["username"]}}\npassword: $${{data.vault_generic_secret.app_secret..data["password"]}}"
+  template = "username: $${data.vault_generic_secret.app_secret.data["username"]}}\npassword: $${{data.vault_generic_secret.app_secret.data["password"]}}"
 }
