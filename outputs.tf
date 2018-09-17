9bdb1f68b168d999d4ab93bc7cf3956b9f43069f
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 18:08:58 +0200

    output again

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "app_secret" {
   description = "secret out of our Vault"
   value = "${data.vault_generic_secret.app_secret.data["username"]}"
 }
-
-output "config_file" {
-  description = "rendered template which consume Vault secrets"
-  value = "${data.template_file.credentials.rendered}"
-}
