ce53204d02b9ff8517e34b8d26566eba884b6eab
Sébastien Braun <sebbraun@gmail.com>
2018-09-17 18:13:42 +0200

    main fixed again

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "vault_generic_secret" "app_secret" {
 }
 
 # Adding a policy
-source "vault_policy" "policy_from_terraform_provider" {
+resource "vault_policy" "policy_from_terraform_provider" {
   name = "policy_from_terraform_provider"
 
   policy = <<EOF
