71ac605f07b2c91395dcde2a073c27ca24559202
Sébastien Braun <sebbraun@gmail.com>
2018-12-12 15:44:35 +0100

    MySQL database description patched

--- db.tf
+++ db.tf
@@ -2,14 +2,20 @@ resource "google_sql_database_instance" "master" {
   name = "vault-instance"
   database_version = "MYSQL_5_7"
   settings {
-    # D0 is the smallest Cloud SQL Tier: 128 MiB RAM / 256 GiB Disk
+    # db-f1-micro tier is the smallest Cloud SQL Tier: 128 MiB RAM / 256 GiB Disk
+    # Second-generation instance tiers are based on the machine
     # to list all available ones: gcloud sql tiers list
-    tier = "D0"
+    tier = "db-f1-micro"
   }
 }
 
 resource "google_sql_database" "users" {
-  name      = "vault"
-  password  = "vpass"
+  name      = "vault-db"
   instance  = "${google_sql_database_instance.master.name}"
+}
+
+resource "google_sql_user" "users" {
+  name     = "vault-user"
+  instance = "${google_sql_database_instance.master.name}"
+  password = "vpass"
 }
