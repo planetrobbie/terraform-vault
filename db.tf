f45c867bb8093bf4e0c8812b301ae8db83ccb0d8
Sébastien Braun <sebbraun@gmail.com>
2018-12-12 23:00:17 +0100

    randomized Cloud SQL instance name to avoid conflict

--- db.tf
+++ db.tf
@@ -1,5 +1,11 @@
+# It's not possible to reuse an instance name for up to a week after deletion.
+# so we randomize the db name to avoid conflicting names.
+resource "random_id" "name" {
+  byte_length = 2
+}
+
 resource "google_sql_database_instance" "master" {
-  name = "${var.db_instance_name}"
+  name = "${var.db_instance_name}-${random_id.name.hex}"
   database_version = "MYSQL_5_7"
   settings {
     # db-f1-micro tier is the smallest Cloud SQL Tier: 128 MiB RAM / 256 GiB Disk
@@ -12,13 +18,31 @@ resource "google_sql_database_instance" "master" {
   }
 }
 
-resource "google_sql_database" "users" {
+resource "google_sql_database" "vault-db" {
   name      = "${var.db_name}"
   instance  = "${google_sql_database_instance.master.name}"
 }
 
-resource "google_sql_user" "users" {
+resource "google_sql_user" "user" {
   name     = "${var.db_user}"
   instance = "${google_sql_database_instance.master.name}"
   password = "${var.db_password}"
+}
+
+resource "vault_mount" "database" {
+  path        = "db"
+  type        = "database"
+  description = "A database secret engine"
+  default_lease_ttl_seconds = "120"
+  max_lease_ttl_seconds = "86400"
+}
+
+resource "vault_database_secret_backend_connection" "mysql" {
+  backend       = "${vault_mount.database.path}"
+  name          = "mysql"
+  allowed_roles = ["ops", "dev"]
+
+  mysql {
+    connection_url = "mysql://${var.db_user}:${var.db_password}@${google_sql_database_instance.master.ip_address.0.ip_address}:3306/${var.db_name}"
+  }
 }
