bcde4cea68400a62830a83a02f53c106e2ec37b2
Sébastien Braun <sebbraun@gmail.com>
2018-12-12 16:53:59 +0100

    location_preference subblock

--- db.tf
+++ db.tf
@@ -1,12 +1,14 @@
 resource "google_sql_database_instance" "master" {
   name = "${var.db_instance_name}"
   database_version = "MYSQL_5_7"
-  zone = "${var.region_zone}"
   settings {
     # db-f1-micro tier is the smallest Cloud SQL Tier: 128 MiB RAM / 256 GiB Disk
     # Second-generation instance tiers are based on the machine
     # to list all available ones: gcloud sql tiers list
     tier = "db-f1-micro"
+    location_preference {
+      zone = "${var.region_zone}"
+    }
   }
 }
 
