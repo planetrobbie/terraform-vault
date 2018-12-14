3e6e7439687d6a4f03bb4654887b1fb2e944a607
Sébastien Braun <sebbraun@gmail.com>
2018-12-14 17:48:52 +0100

    just trying something

--- auth.tf
+++ auth.tf
@@ -1,3 +1,13 @@
 resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
+
+resource "vault_generic_secret" "admin-user" {
+  path = "auth/userpass/users/admin"
+
+  data_json = <<EOT
+{
+  "password": "admin"
+}
+EOT
+}
