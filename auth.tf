4c66433d9a630bccc3e7de6e35de49c321b13a88
Sébastien Braun <sebbraun@gmail.com>
2018-12-14 17:53:29 +0100

    creating local users

--- auth.tf
+++ auth.tf
@@ -2,6 +2,26 @@ resource "vault_auth_backend" "userpass" {
   type = "userpass"
 }
 
+resource "vault_generic_secret" "ops-user" {
+  path = "auth/userpass/users/ops"
+
+  data_json = <<EOT
+{
+  "password": "ops"
+}
+EOT
+}
+
+resource "vault_generic_secret" "dev-user" {
+  path = "auth/userpass/users/dev"
+
+  data_json = <<EOT
+{
+  "password": "dev"
+}
+EOT
+}
+
 resource "vault_generic_secret" "admin-user" {
   path = "auth/userpass/users/admin"
 
--- files/script.tpl
+++ files/script.tpl
@@ -4,10 +4,5 @@ export VAULT_ADDR='${vault_address}'
 export VAULT_TOKEN='${vault_token}'
 export VAULT_CACERT=/etc/vault/tls/ca.crt
 
-# Create Ops user
-/usr/local/bin/vault write auth/userpass/users/ops password=ops
-
-# Create Dev user
-/usr/local/bin/vault write auth/userpass/users/dev password=dev
 
 rm /tmp/script.sh
