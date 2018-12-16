724df2278443fbd4188f17a65e1fbb9c66b4bff4
Sébastien Braun <sebbraun@gmail.com>
2018-12-16 18:40:21 +0100

    transit key more descriptive

--- transit.tf
+++ transit.tf
@@ -11,17 +11,29 @@ resource "vault_generic_secret" "key" {
   data_json = <<EOT
 {
   "exportable": true
+  "allow_plaintext_backup":true,
+  "deletion_allowed":true,
+  "derived":true,
+  "exportable":true,
+  "min_available_version":0,
+  "min_decryption_version":1,
+  "min_encryption_version":0,
+  "supports_decryption":true,
+  "supports_derivation":true,
+  "supports_encryption":true,
+  "supports_signing":true,
+  "type":"aes256-gcm96"
 }
 EOT
 }
 
 # allow key deletion
 # following operation happens but 
-resource "vault_generic_secret" "key-config" {
-  path = "transit/keys/key/config"
-  data_json = <<EOT
-{
-  "deletion_allowed": true
-}
-EOT
-}
+#resource "vault_generic_secret" "key-config" {
+#  path = "transit/keys/key/config"
+#  data_json = <<EOT
+#{
+#  "deletion_allowed": true
+#}
+#EOT
+#}
