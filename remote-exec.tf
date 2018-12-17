29d6cc611c20bc222f3ef47483eecf5116a90908
Sébastien Braun <sebbraun@gmail.com>
2018-12-18 00:06:06 +0100

    Consul-template systemd service

--- remote-exec.tf
+++ remote-exec.tf
@@ -81,7 +81,7 @@ data "template_file" "key" {
 # Do out of band operation on Vault Server v1
 resource "null_resource" "remote-exec" {
   triggers {
-    version = 46,
+    version = 47,
     script = "${data.template_file.script.rendered}",
     playbook = "${data.template_file.playbook.rendered}",
     snippets = "${data.template_file.snippet.rendered}",
@@ -138,6 +138,12 @@ resource "null_resource" "remote-exec" {
   provisioner "file" {
     content      = "${data.template_file.key.rendered}"
     destination = "/tmp/key.tpl"
+  }
+
+  // copy Comnsul-template systemd service over
+  provisioner "file" {
+    source      = "./files/consul-template.service"
+    destination = "/etc/systemd/system/consul-template.service"
   }
 
   // change permissions to executable and pipe its output execution into a new file
