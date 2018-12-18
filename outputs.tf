383e8edd511c6b5097f283d8c5d9c5ee3b912563
Sébastien Braun <sebbraun@gmail.com>
2018-12-19 00:28:20 +0100

    output decoded priv key to check it before injecting in to Vault GCP Auth backend

--- outputs.tf
+++ outputs.tf
@@ -15,6 +15,7 @@ output "mysql_connection_string" {
   value = "${var.enable_secret_engine_db != 0 ? format("mysql://%s:%s@%s:3306/%s", var.db_user, var.db_password, google_sql_database_instance.master.ip_address.0.ip_address, var.db_name) : "Database secret engine not enabled"}"
 }
 
-output "google_creds" {
-  value = "${google_service_account_key.vault-iam-auth-key.public_key}"
+output "google_priv_creds" {
+  value = "${base64decode(google_service_account_key.vault-iam-auth-key.private_key)}"
 }
+
