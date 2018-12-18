1057da9f87e88a17757bc91f85c3806f12c01383
Sébastien Braun <sebbraun@gmail.com>
2018-12-18 10:07:13 +0100

    approle renamed consul-template

--- approle.tf
+++ approle.tf
@@ -2,8 +2,8 @@ resource "vault_auth_backend" "approle" {
   type = "approle"
 }
 
-resource "vault_approle_auth_backend_role" "pki" {
+resource "vault_approle_auth_backend_role" "consul-template" {
   backend   = "${vault_auth_backend.approle.path}"
-  role_name = "pki"
+  role_name = "consul-template"
   policies  = ["default", "pki_int"]
 }
