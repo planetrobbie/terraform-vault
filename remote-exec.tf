66d30318a4b2386f563086c8af02ce94599e2807
Sébastien Braun <sebbraun@gmail.com>
2018-12-18 11:29:54 +0100

    force exec

--- remote-exec.tf
+++ remote-exec.tf
@@ -91,7 +91,7 @@ data "template_file" "vault-agent" {
 # Do out of band operation on Vault Server v1
 resource "null_resource" "remote-exec" {
   triggers {
-    version = 51,
+    version = 52,
     script = "${data.template_file.script.rendered}",
     playbook = "${data.template_file.playbook.rendered}",
     snippets = "${data.template_file.snippet.rendered}",
