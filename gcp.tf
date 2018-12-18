246912d974714077149630ff2eb888d777560260
Sébastien Braun <sebbraun@gmail.com>
2018-12-18 23:55:10 +0100

    GCP IAM role

--- gcp.tf
+++ gcp.tf
@@ -12,13 +12,21 @@ resource "google_service_account" "vault-iam-auth" {
 resource "google_project_iam_member" "vault-iam-auth-token-creator-role" {
   project = "${var.project_name}"
   role    = "roles/iam.serviceAccountTokenCreator"
-  member  = "serviceAccount:${var.project_name}-vault-iam-auth@sb-vault.iam.gserviceaccount.com"
+  member  = "serviceAccount:${var.project_name}-vault-iam-auth@${var.project_name}.iam.gserviceaccount.com"
 }
 
 # Assign Required Role to Service Account
 resource "google_project_iam_member" "vault-iam-auth-key-admin" {
   project = "${var.project_name}"
   role    = "roles/iam.serviceAccountKeyAdmin"
-  member  = "serviceAccount:${var.project_name}-vault-iam-auth@sb-vault.iam.gserviceaccount.com"
+  member  = "serviceAccount:${var.project_name}-vault-iam-auth@${var.project_name}.iam.gserviceaccount.com"
 }
 
+# GCP AUTH IAM Role
+resource "vault_gcp_auth_backend_role" "gcp" {
+    type                   = "iam"
+    backend                = "${vault_auth_backend.gcp.path}"
+    project_id             = "${var.project_name}"
+    bound_service_accounts = ["${var.project_name}-vault-iam-auth@${var.project_name}.iam.gserviceaccount.com"]
+    policies               = ["dev", "ops"]
+}
