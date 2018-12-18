9fbceff3db8cb4161e9b695f6fb60b8721b37c97
Sébastien Braun <sebbraun@gmail.com>
2018-12-18 23:36:06 +0100

    vault-iam-auth-token-creator-role

--- gcp.tf
+++ gcp.tf
@@ -7,3 +7,10 @@ resource "google_service_account" "vault-iam-auth" {
   account_id   = "${var.project_name}-vault-iam-auth"
   display_name = "${var.project_name} Vault IAM Auth Account"
 }
+
+# Assign Required Role to Service Account
+resource "google_project_iam_member" "vault-iam-auth-token-creator-role" {
+  project = "${project_id}"
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${project_id}-vault-iam-auth@sb-vault.iam.gserviceaccount.com"
+}
