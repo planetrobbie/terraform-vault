1285825c5b5c1bbd3fd300d1b3c5be3d419105df
Sébastien Braun <sebbraun@gmail.com>
2018-12-19 22:21:28 +0100

    back away from adding role allowed

--- db.tf
+++ db.tf
@@ -60,7 +60,7 @@ resource "vault_database_secret_backend_connection" "mysql" {
   count = "${var.enable_secret_engine_db}"
   backend       = "${vault_mount.database.path}"
   name          = "mysql"
-  allowed_roles = ["ops", "dev", "all"]
+  allowed_roles = ["ops", "dev"]
   verify_connection = false
 
   mysql {
