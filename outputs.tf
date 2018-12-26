0a7254cf67ea6deb2aea9b34e8f881082e3efd12
Sébastien Braun <sebbraun@gmail.com>
2018-12-26 14:01:17 +0100

    outputs patched

--- outputs.tf
+++ outputs.tf
@@ -17,26 +17,3 @@ output "v2_addrs" {
 output "mysql_connection_string" {
   value = "${var.enable_secret_engine_db != 0 ? format("mysql://%s:%s@%s:3306/%s", var.db_user, var.db_password, google_sql_database_instance.master.ip_address.0.ip_address, var.db_name) : "Database secret engine not enabled"}"
 }
-
-#####################################################################
-# Output for Google Cloud GKE Cluster
-#####################################################################
-output "client_certificate" {
-  value     = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-  sensitive = true
-}
-
-output "client_key" {
-  value     = "${google_container_cluster.primary.master_auth.0.client_key}"
-  sensitive = true
-}
-
-output "cluster_ca_certificate" {
-  value     = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-  sensitive = true
-}
-
-output "host" {
-  value     = "${google_container_cluster.primary.endpoint}"
-  sensitive = true
-}
