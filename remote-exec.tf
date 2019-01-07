0b12a915f700bc685d3ec4e23d9e90af1dd7682e
Sébastien Braun <sebbraun@gmail.com>
2019-01-07 10:45:43 +0100

    remote-exec forced

--- remote-exec.tf
+++ remote-exec.tf
@@ -133,7 +133,7 @@ data "template_file" "bookshelf-config" {
 # Do out of band operation on Vault Server v1
 resource "null_resource" "remote-exec" {
   triggers {
-    version = 67,
+    version = 68,
     script = "${data.template_file.script.rendered}",
     playbook = "${data.template_file.playbook.rendered}",
     snippets = "${data.template_file.snippet.rendered}",
